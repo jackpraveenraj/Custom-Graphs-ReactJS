013ebc5a2b154f15476799b82b55841b517c2655
Jack Praveen Raj Ilango <jackpraveenraj.work@gmail.com>
2021-08-19 05:57:43 +0000

    commit on line chart

--- ospapp/src/components/LineChart.js
+++ ospapp/src/components/LineChart.js
@@ -6,27 +6,36 @@ const LineChart = () => {
         <div>
             
         <Line
+        
         data={{
           labels: ['05/09/21', '06/09/21', '07/09/21', '08/09/21', '09/09/21'],
           
           datasets: [
-            {
-            label: 'Awaken moments',
-            data: [21, 20, 28, 26, 27],
-            backgroundColor: 
-                'rgba(153, 102, 255, 0.2)',
-            borderColor: 
-                'rgba(153, 102, 255, 1)',
-            borderWidth: 1,
-          },
-  
+            
           {
-            label: 'Awaken_min',
+            label: 'Minutes Awake',
             data: [19,50,64,55,51],
-            backgroundColor: 'rgba(255, 157, 0, 0.4)',
-            borderColor: 'rgba(255, 157, 0, 1)',
-            borderWidth: 1,
+            backgroundColor: 'rgba(153, 102, 255, 0.3)',
+            borderColor: 'rgba(153, 102, 255, 1)',
+            borderWidth: 3,
+            fill: {
+              target: 'origin',
+              above: 'rgba(153, 102, 255, 0.3)',   // Area will be red above the origin
+            }
           },
+
+//            {
+//              label: 'Minutes Asleep',
+//              data: [216,287,413,293,329],
+//              backgroundColor: 'rgba(255, 157, 0, 0.4)',
+//              borderColor: 'rgba(255, 157, 0, 1)',
+//              borderWidth: 1,
+//              fill: {
+//                target: 'origin',
+//                above: 'rgba(255, 157, 0, 0.4)',   // Area will be red above the origin
+//              }
+//            },
+          
   
         ],
         }}
